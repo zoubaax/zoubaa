e1f225153daa8ac00798a63726be9d0eeb285ab3
zoubaa mohammed <zoubaax@gmail.com>
2026-03-20 15:28:07 +0000

    feat: Initialize React application with routing and a developer console easter egg.

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,28 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </StrictMode>,
 )
+
+// 🕵️‍♂️ Developer Easter Egg
+if (typeof window !== 'undefined') {
+  // Fetch the gif asynchronously so it doesn't block the initial JS bundle!
+  fetch('/surprise.gif')
+    .then(response => response.blob())
+    .then(blob => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const base64data = reader.result;
+        console.log(
+          "%c ",
+          `font-size: 1px; padding: 180px; background-image: url('${base64data}'); background-size: contain; background-repeat: no-repeat;`
+        );
+        console.log(
+          "%cWhat are you doing in the console? Get out! 🚪💨",
+          "font-size: 18px; font-weight: bold; color: #00d2ff; font-family: sans-serif;"
+        );
+      };
+      reader.readAsDataURL(blob);
+    })
+    .catch(err => {
+      // Ignore errors silently so things don't break
+    });
+}

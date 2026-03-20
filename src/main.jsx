09f3a65fced9b09ecf762374607396a5d60adc8b
zoubaa mohammed <zoubaax@gmail.com>
2026-03-20 14:31:10 +0000

    feat: Initialize React application with StrictMode and BrowserRouter.

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/zoubaa">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
